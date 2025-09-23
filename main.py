19b335fa2d3bfe122b882614d3e406e84be3b79b
moonxplor <saha.snigdha654@gmail.com>
2025-09-23 14:10:41 +0530

    My First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+print("Welcome to the number guessing game!")
+print("You have to guess a number between 1 and 10.")
+Guess=int(input("Enter a nummber between 1 and 10: "))
+if Guess==5:
+    print("Win")
+else:
+    print("Lose")
+
