4c714a1fd99f58b49d412a5a3094d33358c85044
moonxplor <saha.snigdha654@gmail.com>
2025-09-24 20:32:07 +0530

    Commit3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 num=random.randint(1,100)
-print("Welcome to the number guessing game!")
-print("You have to guess a number between 1 and 100")
-guess=int(input("Enter a nummber between 1 and 100"))
 attempts=0
 max_attempts=5
-while True:
+print("Welcome to the number guessing game!") 
+print("You have to guess a number between 1 and 100")
+print("You have 5 attempts")
+guess=int(input("Enter a nummber between 1 and 100"))
+while attempts<max_attempts:
     guess=int(input("Guess the number:"))
     attempts+=1
 if guess==num:
