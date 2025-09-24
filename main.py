6679f8a13cc06969614ce8d834ec4718d433d6c9
moonxplor <saha.snigdha654@gmail.com>
2025-09-24 12:08:34 +0530

    My First Commit

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import random
+num=random.randint(1,100)
 print("Welcome to the number guessing game!")
-print("You have to guess a number between 1 and 10.")
-Guess=int(input("Enter a nummber between 1 and 10: "))
-if Guess==5:
+print("You have to guess a number between 1 and 100")
+guess=int(input("Enter a nummber between 1 and 100"))
+attempts=0
+max_attempts=5
+while True:
+    guess=int(input("Guess the number:"))
+    attempts+=1
+if guess==num:
     print("Win")
+elif guess<num:
+    print("Too low")
+elif guess>num:
+    print("Too high")
 else:
     print("Lose")
 
