8ef5c2b75c1b1be57057ddc1ca9048a95168c70c
moonxplor <saha.snigdha654@gmail.com>
2025-09-26 13:06:30 +0530

    5.

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ max_attempts=5
 print("Welcome to the number guessing game!") 
 print("You have to guess a number between 1 and 100")
 print("You have 5 attempts")
-guess=int(input("Enter a nummber between 1 and 100"))
+print("Good luck!")
+guess=int(input(f"Attempt {attempts+1}: Guess the number:"))
 while attempts<max_attempts:
-    guess=int(input("Guess the number:"))
+    
     attempts+=1
 if guess==num:
     print("Win")
@@ -17,4 +18,5 @@ elif guess>num:
     print("Too high")
 else:
     print("Lose")
+
 
